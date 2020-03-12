476ed9dcf27f811e9c6296015ac17c5d0939a602
Teja9494 <48782618+Teja9494@users.noreply.github.com>
2020-03-12 18:08:03 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Gudiya',         # How you named your package folder (MyLib)
   packages = ['Gudiya'],   # Chose the same as "name"
-  version = '0.1.1.4',      # Start with a small number and increase it with every change you make
+  version = '0.1.1.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Small Resnet 18 example',   # Give a short description about your library
   author = 'Tej',                   # Type in your name
   author_email = 'tejintel@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Teja9494',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Teja9494/Gudiya/archive/0.1.1.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Teja9494/Gudiya/archive/0.1.1.5.tar.gz',    # I explain this later on
   keywords = ['RESNET18', 'BASIC', 'TRAIN','TEST'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'torch',
